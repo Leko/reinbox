3092bdcbb6e80deeb05b165a1c55edf074569319
Leko <leko.noor@gmail.com>
2018-09-24 19:59:48 +0900

    :rotating_light: Export types

--- packages/reinbox/src/index.ts
+++ packages/reinbox/src/index.ts
@@ -1,3 +1,5 @@
 export { InboxProvider as Provider } from "./InboxProvider";
 export { Inbox } from "./Inbox";
 export { withPublish, WithPublishProps } from "./withPublish";
+
+export { Payload, State } from "./context";
