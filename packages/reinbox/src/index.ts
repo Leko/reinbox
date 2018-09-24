942c52ae8bcda49c967d8296db8ee4d46726cf4f
Leko <leko.noor@gmail.com>
2018-09-24 22:10:30 +0900

    :sparkles: Export RawProvider and Context

--- packages/reinbox/src/index.ts
+++ packages/reinbox/src/index.ts
@@ -2,4 +2,4 @@ export { InboxProvider as Provider } from "./InboxProvider";
 export { Inbox } from "./Inbox";
 export { withPublish, WithPublishProps } from "./withPublish";
 
-export { Payload, State } from "./context";
+export { Payload, State, Context, Provider as RawProvider } from "./context";
